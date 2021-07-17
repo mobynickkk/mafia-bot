6ceb1bb3b1ef28cbee822a5947cddd144733ca7c
KirillKonstantinov <73214820+KirillKonstantinov@users.noreply.github.com>
2021-07-18 02:06:38 +0300

    Изменения получения токена и отделение логики бота от костант

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import telebot
 
+token = ''
 with open('token.txt', 'r') as f:
-    body = [line for line in f]
-token = body[0]
+    token = f.readline()
+
 
 bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
