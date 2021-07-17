5420179648ce1d9d6c446082b21c710f6ed11e43
KirillKonstantinov <73214820+KirillKonstantinov@users.noreply.github.com>
2021-07-17 23:58:59 +0300

    Беру токен, создаю бота и обрабатываю команду /start

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+
+with open('token.txt', 'r') as f:
+    body = [line for line in f]
+token = body[0]
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет!')
